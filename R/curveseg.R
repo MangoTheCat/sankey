9add8a88da9de8218a7c6d6bd5df8535e450936d
Gabor Csardi <csardi.gabor@gmail.com>
2015-12-09 15:08:37 +0000

    Whitespace nitpicking [ci skip]

--- R/curveseg.R
+++ R/curveseg.R
@@ -1,7 +1,7 @@
 
 #' @importFrom graphics lines polygon
 
-curveseg <- function(x0, x1, y0, y1, width =  1, nsteps = 50,
+curveseg <- function(x0, x1, y0, y1, width = 1, nsteps = 50,
                      colorstyle, col = "#ffcc0066", grad = NULL, lty = 1,
                      curvestyle = c("sin", "line")) {
 
@@ -17,21 +17,21 @@ curveseg <- function(x0, x1, y0, y1, width =  1, nsteps = 50,
   }
 
   if (curvestyle == "sin" ) {
-    xx  <- seq(-pi/2, pi/2, length.out = nsteps)
+    xx <- seq(-pi/2, pi/2, length.out = nsteps)
     yy <- y0 + (y1 - y0) * (sin(xx) + 1 ) / 2
-    xx <- seq(x0, x1, length.out= nsteps)
+    xx <- seq(x0, x1, length.out = nsteps)
   }
 
   if (curvestyle == "line" ) {
-    xx <- seq(x0, x1, length.out= nsteps)
-    yy <- seq(y0, y1, length.out= nsteps)
+    xx <- seq(x0, x1, length.out = nsteps)
+    yy <- seq(y0, y1, length.out = nsteps)
   }
 
   for(i in 1:(nsteps-1) ) {
     polygon(
       c(xx[i], xx[i + 1], xx[i + 1], xx[i] ),
       c(yy[i], yy[i + 1], yy[i + 1] + w, yy[i] + w ),
-      col = grad[i], border= grad[i]
+      col = grad[i], border = grad[i]
     )
 
     lines(c(xx[i], xx[i + 1]), c(yy[i], yy[i + 1]), lty = lty)
